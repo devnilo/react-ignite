59176d2eefd1de092b0d6d88173c1c83550d8069
Danilo Araujo <dnlysid@gmail.com>
2022-11-15 14:00:05 -0300

    fundamentos react

--- aulas/01-fundamentos-react/src/App.jsx
+++ aulas/01-fundamentos-react/src/App.jsx
@@ -3,8 +3,15 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
+      <Post
+        author="Danilo Araujo"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aut dolores iusto mollitia corporis deleniti dicta quia reiciendis numquam animi molestiae molestias, voluptatem quibusdam inventore, voluptas et? Itaque, nisi voluptas."
+      />
+
+      <Post
+        author="Henrique Dantas"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, iusto. Sit doloremque unde numquam sequi, cumque voluptatum ab fuga molestias harum maiores dicta ea quia temporibus quae commodi praesentium inventore."
+      />
     </div>
   )
 }
-
