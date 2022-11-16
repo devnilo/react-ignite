a022c61da6230d1125a84bf6374b0237a545551c
Danilo Araujo <dnlysid@gmail.com>
2022-11-16 19:57:28 -0300

    criando componente Comment

---
+++ aulas/01-fundamentos-react/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/devnilo.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Danilo Araujo</strong>
+                            <time title="16 de Novembro às 16:49" dateTime="2022-11-16 16:49:36">Cerca de 2h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
