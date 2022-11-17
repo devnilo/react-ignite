d20bdf4cfdf3130a8702aca2318f06407f91ead6
Danilo Araujo <dnlysid@gmail.com>
2022-11-17 19:00:40 -0300

    iterando jsx

--- aulas/01-fundamentos-react/src/App.jsx
+++ aulas/01-fundamentos-react/src/App.jsx
@@ -7,6 +7,42 @@ import styles from './App.module.css';
 import './global.css';
 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/devnilo.png',
+      name: 'Danilo Araujo',
+      role: 'Front-end Developer',
+    },
+
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz junto com o Elon Musk sobre os foguetes. O nome do projeto é Voando Alto 🚀' },
+      { type: 'link', content: '#novoprojeto #spacex #pleno2022' },
+    ],
+
+    publishedAt: new Date('2022-11-16 18:50:23')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/henriquedantass.png',
+      name: 'Henrique Dantas',
+      role: 'Front-end Developer',
+    },
+
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz junto com o Elon Musk sobre os foguetes. O nome do projeto é Voando Alto 🚀' },
+      { type: 'link', content: '#novoprojeto #spacex #pleno2022' },
+    ],
+
+    publishedAt: new Date('2022-11-17 19:32:28')
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -15,10 +51,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Danilo Araujo"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem hic mollitia rem corrupti? Recusandae at corporis iure adipisci natus esse neque explicabo obcaecati! Recusandae totam illo tempora deleniti mollitia rerum."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div >
